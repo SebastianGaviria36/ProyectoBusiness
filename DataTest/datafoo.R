e48de03771177ae77860aeafb7fdfc6e9ba1dd94
PracticanteAutecoBusiness <pautecobusiness@autecomobility.com>
2022-09-29 16:30:52 -0500

    Patching datafoo: setting numeric km in sap set

--- DataTest/datafoo.R
+++ DataTest/datafoo.R
@@ -5,13 +5,12 @@ datafoo <- function(datasap,datatall,datamod){
   library(tidyverse)
   library(readxl)
   library(lubridate)
-  #REQUIRED PACKAGES---------------------------------------
   
   #strings with the names of the excel files---------------
   sap <- read_excel(datasap)
-  tall <- read_excel(datatall)
+  #tall <- read_excel(datatall)
   mod <- read_excel(datamod) 
-  #strings with the names of the excel files---------------
+
   
   
   splitdate <- max(mod$FECHA_INGRESO) #Split date
@@ -26,6 +25,7 @@ datafoo <- function(datasap,datatall,datamod){
   n <- nrow(sap)
   sap <- sap %>%
     mutate(marca = rep("STARK",n),
+           kilometraje = as.numeric(v55),
            tipo_de_servicio = rep("Correctivo",n),
            tipo = rep("GARANTÍAS",n), 
            descripcion_servicio = rep("CORRECTIVO",n),
@@ -38,7 +38,7 @@ datafoo <- function(datasap,datatall,datamod){
            chasis = v15,
            motor = v20,
            placa = v16,
-           kilometraje = v55,
+           kilometraje = kilometraje,
            fecha_ingreso = v49,
            fecha_alistamiento = v77,
            fecha_aom = v77,
@@ -65,12 +65,14 @@ datafoo <- function(datasap,datatall,datamod){
            dias = dias) %>% as.data.frame()
   
   for (i in 1:n){
+    
     sap[i,14] <- sort(unique(mod$VERSION[mod$CHASIS==sap[i,3]]))[1]
     sap[i,16] <- sort(unique(mod$EMPRESA[mod$CHASIS==sap[i,3]]))[1]
     sap[i,23] <- sort(unique(mod$DEPARTAMENTO[mod$CIUDAD==sap[i,24]]))[1]
     sap[i,27] <- sort(unique(mod$CIUDAD_PROPIETARIO[mod$CHASIS==sap[i,3]]))[1]
     sap[i,28] <- sort(unique(mod$DEPARTAMENTO_PROPIETARIO[mod$CHASIS==sap[i,3]]))[1]
     sap[i,29] <- sort(unique(mod$RENTING[mod$CHASIS==sap[i,3]]))[1]
+    
     if (is.na(sap[i,5])){
       sap[i,5] <- sort(unique(mod$PLACA[mod$CHASIS==sap[i,3]]))[1]
     }
@@ -82,7 +84,6 @@ datafoo <- function(datasap,datatall,datamod){
       sap[i,10] <- tallersap
     }
     else{
-      
       print(data.frame(Taller=talleres))
       print(data.frame(TALLERSAP=tallersap))
       tallerindex <- readline("Reemplazar por: ")
@@ -90,11 +91,11 @@ datafoo <- function(datasap,datatall,datamod){
       if (is.na(as.integer(tallerindex))){
         sap[i,10] <- tallerindex
       }
+      
       else{
         sap[i,10] <- talleres[as.integer(tallerindex)]
       }
     }
-    
     
     fallas <- unique(mod$DESCRIPCION_PARTE)
     fallasap <- sap[i,12]
@@ -116,15 +117,13 @@ datafoo <- function(datasap,datatall,datamod){
         sap[i,c(12,19)] <- fallas[as.integer(fallaindex)] 
       }
     }
-    
   }
   names(sap) <- toupper(names(sap))
   View(sap)
   View(rbind(mod,sap))
-  #SAP TREATMENT-------------------------------------------
-  
+
   #TALLER TREATMENT----------------------------------------
-  #TALLER TREATMENT----------------------------------------
+
 }
 
 datafoo(datasap = "rep4w.XLSX", datamod = "datosModelo2.xlsx")
